21f4692d010e3720bf1655d4b063f049e21ff3fe
Sebastian Schmittner <schmittner@eecc.info>
2018-10-02 15:51:37 +0200

    use localhost fallback by default, only change when running in docker

--- docker_run.sh
+++ docker_run.sh
@@ -3,6 +3,11 @@ set -e
 
 cd /usr/src/app
 
+FALLBACK_IP=192.168.20.41
+
+echo "Using fallback ip ${FALLBACK_IP} for the web3 provider"
+sed -i -e "s/127\.0\.0\.1/${FALLBACK_IP}/" src/utils/getWeb3.js
+
 truffle migrate --network ganache_docker
 
 npm start
