5a3cb7551aba02640696f5d4743ad45955044873
Sebastian Schmittner <schmittner@eecc.info>
2018-10-09 12:23:54 +0200

    removed comments

--- eecc_chain_app/src/Explorer.js
+++ eecc_chain_app/src/Explorer.js
@@ -95,47 +95,6 @@ class Explorer extends Component {
     )
   }
 
-  // render() {
-  //   return (
-  //     <div className="Balance">
-  //
-  //       <main className="container">
-  //         <div className="pure-g">
-  //           <div className="pure-u-1-1">
-  //           <h1>EECC Chain Explorer</h1>
-  //           <table>
-  //           <tbody>
-  //           <tr>
-  //           <th>
-  //           Block Number
-  //           </th>
-  //           <th>
-  //           Transactions
-  //           </th>
-  //           </tr>
-  //           {this.state.blocks.map((block) =>
-  //           <tr key={block.hash}>
-  //                                  <td>{block.number}</td>
-  //                                  <td>
-  //                                  <ul>
-  //                                  {
-  //                                      block.transactions.map((tx, index) =>
-  //                                                             <li key={index}>{decoded_transaction_to_str(this.props.web3, tx.from, this.props.abiDecoder.decodeMethod(tx.input))}</li>
-  //                                                            )}
-  //                                  </ul>
-  //                                  </td>
-  //           </tr>
-  //           )}
-  //
-  //           </tbody>
-  //           </table>
-  //
-  //          </div>
-  //         </div>
-  //       </main>
-  //     </div>
-  //   );
-  // }
 }
 
 export default Explorer
