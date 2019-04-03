06c0e970f0f882c5392da1814d967472bd3b70f1
Sebastian Schmittner <schmittner@eecc.info>
2019-04-03 21:27:07 +0200

    skip fallback ip replacement if env var not set

--- eecc_chain_app/docker_run.sh
+++ eecc_chain_app/docker_run.sh
@@ -3,7 +3,7 @@ set -e
 
 cd /usr/src/app
 
-if [ -z "$var" ]; then 
+if [ ! -z "$FALLBACK_IP" ]; then 
   echo "Using fallback ip ${FALLBACK_IP} for the web3 provider"
   sed -i -e "s/127\.0\.0\.1/${FALLBACK_IP}/" src/utils/getWeb3.js
 fi
